bb6c2e961cdc4d6b1e8ddd121a9a17f379c2b3fe
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-09-02 12:53:39 +0300

    v0.8

--- src/components/caroussel.tsx
+++ src/components/caroussel.tsx
@@ -35,6 +35,92 @@ const slides = [
     title: "Désert Infini",
     description: "Ressentez la magie du désert sous un ciel étoilé",
   },
+
+  {
+    id: 5,
+    image:
+      "https://res.cloudinary.com/dbpoyo4gw/image/upload/v1756803546/20_obcxai.jpg",
+    title: "Paysage Montagneux",
+    description: "Découvrez la beauté naturelle des montagnes majestueuses",
+  },
+  {
+    id: 6,
+    image:
+      "https://res.cloudinary.com/dbpoyo4gw/image/upload/v1756803546/19_xbhssq.jpg",
+    title: "Océan Tropical",
+    description: "Plongez dans les eaux cristallines des tropiques",
+  },
+  {
+    id: 7,
+    image:
+      "https://res.cloudinary.com/dbpoyo4gw/image/upload/v1756803546/18_hrsx1n.jpg",
+    title: "Forêt Mystique",
+    description: "Explorez les secrets cachés de la forêt enchantée",
+  },
+  {
+    id: 8,
+    image:
+      "https://res.cloudinary.com/dbpoyo4gw/image/upload/v1756803546/17_azhgia.jpg",
+    title: "Désert Infini",
+    description: "Ressentez la magie du désert sous un ciel étoilé",
+  },
+  {
+    id: 9,
+    image:
+      "https://res.cloudinary.com/dbpoyo4gw/image/upload/v1756803546/16_khyeyy.jpg",
+    title: "Paysage Montagneux",
+    description: "Découvrez la beauté naturelle des montagnes majestueuses",
+  },
+  {
+    id: 10,
+    image:
+      "https://res.cloudinary.com/dbpoyo4gw/image/upload/v1756803545/14_diwgpe.jpg",
+    title: "Océan Tropical",
+    description: "Plongez dans les eaux cristallines des tropiques",
+  },
+  {
+    id: 11,
+    image:
+      "https://res.cloudinary.com/dbpoyo4gw/image/upload/v1756803545/13_rdtqrl.jpg",
+    title: "Forêt Mystique",
+    description: "Explorez les secrets cachés de la forêt enchantée",
+  },
+  {
+    id: 12,
+    image:
+      "https://res.cloudinary.com/dbpoyo4gw/image/upload/v1756803545/15_rchmmu.jpg",
+    title: "Désert Infini",
+    description: "Ressentez la magie du désert sous un ciel étoilé",
+  },
+
+  {
+    id: 13,
+    image:
+      "https://res.cloudinary.com/dbpoyo4gw/image/upload/v1756803545/12_acj5lj.jpg",
+    title: "Océan Tropical",
+    description: "Plongez dans les eaux cristallines des tropiques",
+  },
+  {
+    id: 14,
+    image:
+      "https://res.cloudinary.com/dbpoyo4gw/image/upload/v1756803545/7_eiem1e.jpg",
+    title: "Forêt Mystique",
+    description: "Explorez les secrets cachés de la forêt enchantée",
+  },
+  {
+    id: 15,
+    image:
+      "https://res.cloudinary.com/dbpoyo4gw/image/upload/v1756803545/10_hmgbu6.jpg",
+    title: "Désert Infini",
+    description: "Ressentez la magie du désert sous un ciel étoilé",
+  },
+  {
+    id: 16,
+    image:
+      "https://res.cloudinary.com/dbpoyo4gw/image/upload/v1756803544/1_rzozim.jpg",
+    title: "Désert Infini",
+    description: "Ressentez la magie du désert sous un ciel étoilé",
+  },
 ];
 
 export default function Carousel() {
